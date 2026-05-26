255f0407b2ea58db4bdb989d839c0a9b6c7ce3ea
aaa <aaa@aaa>
2026-05-26 11:49:02 +0900

    mapper用意

---
+++ src/main/java/com/example/app/mapper/ClothMapper.java
@@ -0,0 +1,27 @@
+package com.example.app.mapper;
+
+import java.util.List;
+
+import org.apache.ibatis.annotations.Mapper;
+
+import com.example.app.domain.Cloth;
+
+@Mapper
+public interface ClothMapper {
+
+	//最新順で洋服表示
+	List<Cloth> findAllDesc();
+
+	//洋服を登録する機能
+	void add(Cloth cloth);
+
+	//登録した洋服を削除する機能
+
+	//カテゴリ別の検索機能
+
+	//お気に入りリスト追加
+
+	//売りたいリスト追加
+
+	//コーディネート登録
+}
