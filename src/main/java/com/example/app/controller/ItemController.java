7702299f93a3ce6079326211e70e5a72c6120664
aaa <aaa@aaa>
2026-05-26 14:00:49 +0900

    controller作成

---
+++ src/main/java/com/example/app/controller/ItemController.java
@@ -0,0 +1,41 @@
+package com.example.app.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import com.example.app.domain.Cloth;
+import com.example.app.mapper.ClothMapper;
+
+import lombok.RequiredArgsConstructor;
+
+@Controller
+@RequiredArgsConstructor
+
+public class ItemController {
+	//フィールド変数準備(mapper)
+	private final ClothMapper mapper;
+	/*@RequiredArgsConstructor書かない場合
+	 *public ItemController(ClothMapper mapper) {
+	        this.mapper = mapper;
+	    }を手書きで書く*/
+
+	//初期ページ表示
+	@GetMapping("/cloth")
+	public String startPage(Model model) {
+		System.out.println("データ取得テスト: " + mapper.findAllDesc());
+		model.addAttribute("cloths", mapper.findAllDesc());
+		return "index";
+	}
+
+	//服の登録処理実行（フォームの送信ボタンが押されたらここに来る）
+	@PostMapping("/add") // ① URLは登録用の名前にする
+	//②引数Clothにしたら全部受け取れる
+	public String addcloth(Cloth cloth) {
+		mapper.add(cloth);//③mapperよんで保存
+		//★redirect:の後ろは(HTMLのファイル名)ではなく=コントローラーのURL
+		return "redirect:/cloth";//④ 保存が終わったら一覧ページ（/cloth）に戻る
+	}
+
+}
