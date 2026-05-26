b58c450d4b54a4ad198b31bbba5f9c3fa2b13264
aaa <aaa@aaa>
2026-05-26 15:43:49 +0900

    トップページ作成

--- src/main/java/com/example/app/controller/ItemController.java
+++ src/main/java/com/example/app/controller/ItemController.java
@@ -4,6 +4,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.example.app.domain.Cloth;
 import com.example.app.mapper.ClothMapper;
@@ -25,14 +27,15 @@ public class ItemController {
 	@GetMapping("/cloth")
 	public String startPage(Model model) {
 		System.out.println("データ取得テスト: " + mapper.findAllDesc());
-		model.addAttribute("cloths", mapper.findAllDesc());
+		model.addAttribute("clothes", mapper.findAllDesc());
 		return "index";
 	}
 
 	//服の登録処理実行（フォームの送信ボタンが押されたらここに来る）
 	@PostMapping("/add") // ① URLは登録用の名前にする
 	//②引数Clothにしたら全部受け取れる
-	public String addcloth(Cloth cloth) {
+	public String addcloth(Cloth cloth,
+			@RequestParam("clothImage") MultipartFile file) {
 		mapper.add(cloth);//③mapperよんで保存
 		//★redirect:の後ろは(HTMLのファイル名)ではなく=コントローラーのURL
 		return "redirect:/cloth";//④ 保存が終わったら一覧ページ（/cloth）に戻る
